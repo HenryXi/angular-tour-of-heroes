bfba7d38b9a83c92b7a0d2d6f0c80a12aa51a15f
yong <xxy668@foxmail.com>
2018-08-08 07:09:01 +0800

    I can use angular display simple page ;)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import {Component, OnInit} from '@angular/core';
+import {HEROES} from '../mock-heroes';
+import {Hero} from "../hero";
 
 @Component({
-    selector: 'app-heroes',
-    templateUrl: './heroes.component.html',
-    styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-};
+  heroes = HEROES;
 
-    constructor() { }
+  constructor() {
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
+  selectedHero: Hero;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
